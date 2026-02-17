6f3ddb48e86d1a1fb77249b3d40ab04ba33420f7
Andreja Simonović <andreja.simonovic19@gmail.com>
2026-02-17 19:04:32 +0100

    * Added budget with maximum and minimum required

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import json
+import sys
 
 user = None
+max_budget = 100000
 
 with open("data/users.json", 'r') as file:
     user = json.load(file)
 
+
+if user['budget'] >= max_budget:
+    print("Imate veći budžet od maksimalnog dozvoljenog")
+    sys.exit()
+elif user['budget'] < 0:
+    print("Nedovoljan iznos budžeta")
+    sys.exit()
